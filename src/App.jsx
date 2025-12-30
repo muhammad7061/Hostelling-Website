0d6116a753cadfcd87307503c060ae120d5705a7
Mohamed Ali Ahmed <mohamed.ali.ahmed.study@gmail.com>
2025-12-30 06:39:42 -0800

    Setting up folder structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Layout Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// Page Components
+import Home from './pages/Home';
+import JoinHI from './pages/JoinHI';
+import Blog from './pages/Blog';
+import Mission from './pages/Mission';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-green-600'>Test</h1>
-    </div>
-  )
-}
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        
+        {/* Main content area where pages swap out */}
+        <main className="main-content">
+          <Routes>
+            {/* Pages */}
+            <Route path="/" element={<Home />} />
+            <Route path="/mission" element={<Mission />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/join" element={<JoinHI />} />
+            
+            {/* 404 Route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
 
-export default App
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/JoinHI.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JoinHI = () => {
+  return (
+    <div>
+      <h1>Join HI</h1>
+    </div>
+  )
+}
+
+export default JoinHI
