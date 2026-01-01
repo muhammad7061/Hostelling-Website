ca064e618790fcb230e1843b62e4147361208a6d
Mohamed Ali Ahmed <mohamed.ali.ahmed.study@gmail.com>
2026-01-01 10:44:24 -0800

    Folder structure(data and some features)

---
+++ src/components/BlogHero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogHero = () => {
+  return (
+    <div>
+      <h1>Blog Hero</h1>
+    </div>
+  )
+}
+
+export default BlogHero
---
+++ src/data/Content.js
@@ -0,0 +1,267 @@
+export const blogs = [
+  // Blog 1
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/12/Peneda-Geres-National-Park-Blog-post-images-1200-x-800-px.png",
+    title: "Uncover The Heart and Soul of Portugal with Hostelling International",
+    date: "December 17, 2025",
+    comments: 0,
+    excerpt: `
+      Ready for your next winter or spring adventure with Hostelling InternationaI (HI)? 
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover 
+      the heart and soul of Portugal – where history, culture, and natural beauty come
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 2
+  {
+    id: "naturaleza-conectada",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/12/Naturaleza-Conectada_PIC-1.jpeg",
+    title:
+      "Naturaleza Conectada – Smart, Accessible & Positive Impact Ecotourism",
+    date: "December 5, 2025",
+    comments: 0,
+    excerpt: `
+      “Naturaleza Conectada” is an innovative project that combines nature, technology, accessibility, and responsible tourism. 
+      Led by a consortium formed by REAJ (the Spanish Youth Hostels Network), Tur4all, SEO/BirdLife, and iUrban, 
+      the initiative forms part of Spain’s national tourism programme “Experiencias Turismo
+      `,
+    content: `
+      “Naturaleza Conectada” is an innovative project that combines nature, technology,
+      accessibility, and responsible tourism...
+    `
+  },
+  // Blog 3
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2018/10/Blog-post-images-1200-x-800-px-1920x1280.png",
+    title: "⛄Festive Fun For Everyone 🌞",
+    date: "December 5, 2025",
+    comments: 0,
+    excerpt: `
+      Ready for your next winter adventure in 2025? Whether you’re dreaming of sparkling snowy escapes, 
+      vibrant winter festivals, or a sunny break by the sea, there’s a world of destinations waiting for you. 
+      Hostelling International (HI) is here to open
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 4
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/11/Denmark-Newsletter1200-x-800-px.png",
+    title: "Top Danish Destinations You Can’t Miss",
+    date: "November 13, 2025",
+    comments: 0,
+    excerpt: `
+      Dreaming of a Scandinavian escape? Denmark is a country where modern design meets the cosy concept of hygge—that uniquely Danish feeling of warmth, 
+      comfort, and togetherness. Every corner tells a story, from colourful harbours and Viking heritage to cycle-friendly streets
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 5
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/10/Untitled-800-x-419-px-6.png",
+    title: "Stay Connected, Travel Freely, For Seamless Global Connectivity",
+    date: "October 17, 2025",
+    comments: 0,
+    excerpt: `
+      Airalo is redefining how travellers stay connected—making global mobile access simpler, smarter, and more affordable. 
+      At Hostelling International, we believe that travel should be accessible, empowering, and connected. 
+      Whether you’re backpacking through Asia, hopping between European hostels, or road-tripping across your own country,
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 6
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/10/Leuven-Flanders-Region-Free-Canva-Teams-1200-x-800-px.png",
+    title: "Say HI to Belgium, One Hostel at a Time",
+    date: "October 9, 2025",
+    comments: 0,
+    excerpt: `
+      Belgium is full of surprises—charming cities, coastal escapes, buzzing student life, and rich history all packed into one compact country. 
+      If you’re looking for a way to explore it all while staying connected to local culture, Hostelling International (HI) offers a brilliant
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 7
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/08/Newsletter-graphics-1200-x-800-px-2.png",
+    title: "Play For Peace: Time To Level Up!",
+    date: "August 28, 2025",
+    comments: 0,
+    excerpt: `
+      PEACE Feels like a big word. Too big sometimes. Like something reserved for world leaders or breaking news headlines. 
+      Just too far off and too hard to reach. But what if peace didn’t have to be some distant, daunting goal? What
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 8
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/08/photo-collage-map-USA.png",
+    title: "East To West USA Adventures With Hostelling International",
+    date: "August 21, 2025",
+    comments: 0,
+    excerpt: `
+      Embarking on a cross-country USA adventure is a dream for many travellers but, with Hostelling International (HI), 
+      you’re guaranteed a journey rich in culture, community, and unforgettable experiences. 
+      From the historic streets of Boston to the soulful rhythms of New Orleans,
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 9
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/07/Digital-Nomad-Work-Table-Free-for-Canva-Teams.png",
+    title: "Our Top Eco-Friendly Remote Work Hubs",
+    date: "August 14, 2025",
+    comments: 0,
+    excerpt: `
+      Hey digital nomads, are you ready to upgrade your office view for that next business video call? Well look no further, 
+      we at Hostelling International (HI) are here to help and have rounded up some of our most unique destinations,
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 10
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/07/Woman-TRavelling-In-Switzerland-Free-forCanva.png",
+    title: "Summer Adventures In Switzerland",
+    date: "July 24, 2025",
+    comments: 0,
+    excerpt: `
+      Switzerland transforms into a paradise for outdoor enthusiasts during the summer months. 
+      And with Hostelling International (HI) by your side, you’re all set to plan those next unforgettable summer adventures in Switzerland! 
+      From alpine trails to crystal-clear lakes, this stunning
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 11
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/07/Untitled-800-x-419-px-21.png",
+    title: "Discover The Freedom Of Exploring Without Luggage – Bounce",
+    date: "July 16, 2025",
+    comments: 0,
+    excerpt: `
+      Are you tired of lugging your bags around while trying to enjoy your travels? Imagine exploring a new city unburdened, 
+      with the freedom to roam and discover without the hassle of heavy luggage. Thanks to our partnership with Bounce, you can
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 12
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/06/Lake-and-Mountain-Free-Canva-Banff-National-Park.png",
+    title: "A Backpacker’s Dream Across Canada",
+    date: "June 16, 2025",
+    comments: 0,
+    excerpt: `
+      Craving the wild? Then let Hostelling International (HI) take you on an epic trip through one of 
+      the world’s most breath-taking and diverse landscapes, Canada. 
+      From the salty Atlantic breeze in Halifax to the glacier-carved serenity of Lake Louise, this
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 13
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/05/1200-X-400-site-web-8-1.png-c.png.webp",
+    title: "Discover France’s Best Regions With Hostelling International",
+    date: "May 28, 2025",
+    comments: 0,
+    excerpt: `
+      France offers a rich tapestry of culture, history, and natural beauty so let Hostelling International (HI) 
+      take you on an unforgettable journey of La Belle France. From the rugged shores of Brittany to picturesque Provence, 
+      the gourmet delights of Nouvelle
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 14
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/05/Untitled-800-x-419-px-17.png",
+    title: "Experience The World On A Budget With FREETOUR.COM",
+    date: "May 23, 2025",
+    comments: 0,
+    excerpt: `
+      At Hostelling International, we know that the best travel experiences don’t have to come with a hefty price tag. 
+      That’s why we’re excited to spotlight our partner, FREETOUR.com, a platform that’s revolutionising how travellers 
+      explore the world. FREETOUR.com is a
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+  // Blog 15
+  {
+    id: "uncover-portugal",
+    img: "https://blog.hihostels.com/wp-content/uploads/2025/04/Mountain-Hiking-Boots-Free-for-Canva.png",
+    title: "Sustainable Trails You Need to Explore",
+    date: "May 8, 2025",
+    comments: 0,
+    excerpt: `
+      Come on an adventure with Hostelling International (HI) and unearth sustainable trails you need to explore, 
+      designed to protect the environment while giving you a fantastic outdoor experience. Whether hiking or cycling, 
+      exploring the great outdoors is a fantastic way
+      `,
+    content: `
+      Ready for your next winter or spring adventure with Hostelling International (HI)?
+      Together with Pousadas de Juventude, let us take you beyond the familiar to uncover
+      the heart and soul of Portugal – where history, culture, and natural beauty come together.
+    `
+  },
+];
